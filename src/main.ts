37d504e07ce832352d3d17e1553ff03c8a8a1988
Abasol <ramzeeesss666@gmail.com>
2024-09-18 21:57:05 +0800

    country options added

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,77 @@
+// country api
+// https://documenter.getpostman.com/view/1134062/T1LJjU52
 
+const APIKEY: string = "60f06bb755213c4c8e5887d8f7b59046";
 
-const APIKEY:string = "60f06bb755213c4c8e5887d8f7b59046" 
+// const weatherLink: string = `https://api.openweathermap.org/data/2.5/weather?q=cebu&appid=${APIKEY}`;
 
+const weatherLink: string = `https://countriesnow.space/api/v0.1/countries`;
 
-const weatherLink: string = `https://api.openweathermap.org/data/2.5/weather?q=cebu&appid=${APIKEY}`
-
-
-interface ResponseObject {
-    status:string;
-    data: any
+interface weatherObject {
+  test: string;
+  err: string;
 }
 
-
-async function testFunction(url:string): Promise<ResponseObject> {
-        try {
-            const test = fetch(url)
-            const result = await test
-            const results = await result.json()
-            // console.log(results)
-            return {
-                status: 'success',
-                data: await results
-            }
-        } catch {
-            return {
-                status: 'success',
-                data:"Error bitch"
-            }
-        }
+interface countryCityName {
+  country: string;
+  city: string;
 }
 
-const test = testFunction(weatherLink)
-
-async function callbackTest (val:object) {
-    console.log(val)
+async function weatherFunction(url: string): Promise<weatherObject> {
+  try {
+    const test = fetch(url);
+    const result = await test;
+    const results = await result.json();
+    console.log(results);
+    return {
+      test: results.name,
+      err: "error bitch",
+    };
+  } catch {
+    return {
+      test: "City is missing",
+      err: "Sup bitch",
+    };
+  }
 }
 
-callbackTest(test)
+async function cityFunction(url: string): Promise<countryCityName> {
+  try {
+    const selectCountry: HTMLElement | null =
+      document.getElementById("selectCountry");
 
+    const countryCityFetch = fetch(url);
+    const countryResp = await countryCityFetch;
+    const data = await countryResp.json();
+    let optionArray: HTMLOptionElement[] = [];
 
+    if (selectCountry) {
+      for (let i = 0; i < data.data.length; i++) {
+        const countryName: string = data.data[i].country;
+        const countryISO: string = data.data[i].iso2;
+        const optionSelect = document.createElement("option");
 
+        optionSelect.setAttribute("value", countryISO);
+        optionSelect.textContent = countryName;
+        optionArray.push(optionSelect);
+      }
 
+      optionArray.forEach((el) => {
+        selectCountry.appendChild(el);
+      });
+    }
+
+    return {
+      country: "Test",
+      city: "String",
+    };
+  } catch {
+    return {
+      country: "Test",
+      city: "String",
+    };
+  }
+}
+cityFunction(weatherLink);
+
+//inputs
