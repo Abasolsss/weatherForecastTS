721934e57febc62abf83abf538ed6ec93c86390c
Abasol <ramzeeesss666@gmail.com>
2024-09-26 23:04:51 +0800

    interface for final value added

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 // country api
 // https://documenter.getpostman.com/view/1134062/T1LJjU52
-const newWeather: HTMLButtonElement | null = document.querySelector(".newWeather")
-
+const newWeather: HTMLButtonElement | null =
+  document.querySelector(".newWeather");
 
 const selectCountry: HTMLElement | null =
-document.getElementById("selectCountry");
+  document.getElementById("selectCountry");
 
+const loadingDiv: HTMLDivElement | null =
+  document.querySelector(".loadingDivHide");
 
-const loadingDiv: HTMLDivElement | null = document.querySelector(".loadingDivHide")
-
-
-
-const inputText = document.getElementById(
-  "inputText"
-) as HTMLInputElement;
+const inputText = document.getElementById("inputText") as HTMLInputElement;
 
 interface weatherObject {
   resp: string;
@@ -21,27 +17,25 @@ interface weatherObject {
 }
 
 interface countryCityName {
-  data: any
+  data: any;
 }
 
-
 //Show modal
-const modalBtn: HTMLButtonElement | null = document.querySelector(".checkWeatherModal")
+const modalBtn: HTMLButtonElement | null =
+  document.querySelector(".checkWeatherModal");
 
-const modalClose: HTMLDivElement | null = document.querySelector(".modalClose")
-const modalDiv: HTMLDivElement | null = document.querySelector(".modalHide")
-
-
+const modalClose: HTMLDivElement | null = document.querySelector(".modalClose");
+const modalDiv: HTMLDivElement | null = document.querySelector(".modalHide");
 
 modalBtn?.addEventListener("click", () => {
-    modalDiv?.classList.remove("modalHide")
-    modalDiv?.classList.add("modalShow")
-})
+  modalDiv?.classList.remove("modalHide");
+  modalDiv?.classList.add("modalShow");
+});
 
-modalClose?.addEventListener("click",() => {
-  modalDiv?.classList.remove("modalShow")
-    modalDiv?.classList.add("modalHide")
-})
+modalClose?.addEventListener("click", () => {
+  modalDiv?.classList.remove("modalShow");
+  modalDiv?.classList.add("modalHide");
+});
 
 async function cityFunction(): Promise<countryCityName> {
   try {
@@ -51,7 +45,7 @@ async function cityFunction(): Promise<countryCityName> {
     const countryResp = await countryCityFetch;
     const data = await countryResp.json();
     return {
-      data: data
+      data: data,
     };
   } catch {
     return {
@@ -60,58 +54,55 @@ async function cityFunction(): Promise<countryCityName> {
   }
 }
 
-
-async function weatherFunction(city:string,country:string): Promise<weatherObject> {
+async function weatherFunction(
+  city: string,
+  country: string
+): Promise<weatherObject> {
   try {
-   
-    const callBack = await cityFunction()
-    const data = callBack.data
+    const callBack = await cityFunction();
+    const data = callBack.data;
 
-  let optionArray: HTMLOptionElement[] = [];
+    let optionArray: HTMLOptionElement[] = [];
 
-  if (selectCountry) {
-    for (let i = 0; i < data.data.length; i++) {
-      const countryName: string = data.data[i].country;
-      const countryISO: string = data.data[i].iso2;
-      const optionSelect = document.createElement("option");
+    if (selectCountry) {
+      for (let i = 0; i < data.data.length; i++) {
+        const countryName: string = data.data[i].country;
+        const countryISO: string = data.data[i].iso2;
+        const optionSelect = document.createElement("option");
 
-      optionSelect.setAttribute("value", countryISO);
-      optionSelect.textContent = countryName;
-      optionArray.push(optionSelect);
+        optionSelect.setAttribute("value", countryISO);
+        optionSelect.textContent = countryName;
+        optionArray.push(optionSelect);
+      }
+
+      optionArray.forEach((el) => {
+        selectCountry.appendChild(el);
+      });
     }
 
-    optionArray.forEach((el) => {
-      selectCountry.appendChild(el);
-    });
-  }
+    if (selectCountry instanceof HTMLSelectElement) {
+      selectCountry.addEventListener("change", () => {
+        newWeather?.classList.add("checkWeatherBtn");
+        newWeather?.classList.remove("newWeather");
+        if (newWeather) {
+          newWeather.disabled = false;
+        }
+        inputText.disabled = false;
+        inputText.setAttribute("placeholder", "Enter City");
+      });
+    }
 
+    const APIKEY: string = "60f06bb755213c4c8e5887d8f7b59046";
 
-  if (selectCountry instanceof HTMLSelectElement) {
- 
-    selectCountry.addEventListener("change", () => {
-      newWeather?.classList.add("checkWeatherBtn")
-      newWeather?.classList.remove("newWeather")
-      if(newWeather) {
-        newWeather.disabled = false;
-      }
-      inputText.disabled = false;
-      inputText.setAttribute("placeholder", "Enter City")
+    const WeatherLink: string = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIKEY}`;
 
-    });
-  }
-
-  const APIKEY: string = "60f06bb755213c4c8e5887d8f7b59046";
-
-
-  const WeatherLink: string = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIKEY}`
-
-
-  const test = fetch(WeatherLink);
-  const result = await test;
-  const results = await result.json();
-      return {
+    // console.log(WeatherLink);
+    const test = fetch(WeatherLink);
+    const result = await test;
+    const results = await result.json();
+    return {
       resp: results,
-      mess: "Success Result"
+      mess: "Success Result",
     };
   } catch {
     return {
@@ -122,45 +113,64 @@ async function weatherFunction(city:string,country:string): Promise<weatherObjec
 }
 
 // to run the async function
-weatherFunction()
+weatherFunction();
 
-const loadingMessage: string[] = ["Loading weather", "Please wait", "Almost Done"]
+const loadingMessage: string[] = [
+  "Loading weather",
+  "Please wait",
+  "Almost Done",
+];
 
-const loadingEl: HTMLElement | null  = document.querySelector(".loadingMessage")
+const loadingEl: HTMLElement | null = document.querySelector(".loadingMessage");
 
-const checkWeatherBtn: HTMLElement | null = document.getElementById
-("showWeather")
+const checkWeatherBtn: HTMLElement | null =
+  document.getElementById("showWeather");
 
-const showResultId: HTMLElement | null = document.getElementById("resultId")
+const showResultId: HTMLElement | null = document.getElementById("resultId");
 
-
-if(checkWeatherBtn instanceof HTMLButtonElement) {
-  const newBtn = checkWeatherBtn
+if (checkWeatherBtn instanceof HTMLButtonElement) {
+  const newBtn = checkWeatherBtn;
   newBtn.addEventListener("click", async () => {
-    const test:string = inputText.value
-    loadingDiv?.classList.remove("loadingDivHide")
-    loadingDiv?.classList.add("loadingDiv")
-    modalDiv?.classList.remove("modalShow")
-    modalDiv?.classList.add("modalHide")
+    const test: string = inputText.value;
+    loadingDiv?.classList.remove("loadingDivHide");
+    loadingDiv?.classList.add("loadingDiv");
+    modalDiv?.classList.remove("modalShow");
+    modalDiv?.classList.add("modalHide");
 
+    let init: number = 0;
 
-    let testNumber: number[] = []
-    let init:number = 0
-   
-  const resultFunc = () => {
-    
-    if(init !== loadingMessage.length) {
-      if(loadingEl) {
-        loadingEl.textContent = loadingMessage[init++]
+    const resultFunc = () => {
+      if (init !== loadingMessage.length) {
+        if (loadingEl) {
+          loadingEl.textContent = loadingMessage[init++];
+        }
+      } else {
+        loadingDiv?.classList.remove("loadingDiv");
+        loadingDiv?.classList.add("loadingDivHide");
       }
-    } else {
-      loadingDiv?.classList.remove("loadingDiv")
-      loadingDiv?.classList.add("loadingDivHide")
-      showResultId?.classList.remove("hideResult")
-      showResultId?.classList.add("showResult")
-      if(showResultId) {
-        showResultId.innerHTML = 
-        `
+      return init;
+    };
+
+    const myInterval = setInterval(resultFunc, 1500);
+
+    interface returnVal {
+      mess: string;
+      resp: {
+        base: string;
+      };
+    }
+
+    const dataVal: returnVal[] = [];
+
+    setTimeout(() => {
+      if (myInterval > 0) {
+        console.log(dataVal[0].mess);
+        console.log(dataVal[0].resp.base);
+        clearInterval(myInterval);
+        showResultId?.classList.remove("hideResult");
+        showResultId?.classList.add("showResult");
+        if (showResultId) {
+          showResultId.innerHTML = `
          <div class="resultHandler">
                 <div class="resultFirstDiv">
                   <span>Results for: Location Here</span>
@@ -173,34 +183,22 @@ if(checkWeatherBtn instanceof HTMLButtonElement) {
                   <span>Weather status</span>
                 </div>
               </div>
-        `
+        `;
+        }
+      } else {
+        console.error("Failed to clear the interval");
       }
-    }
-    return init
-  } 
+    }, 6000);
 
-  const myInterval = setInterval(resultFunc,1500)
-
-  setTimeout(() => {
-    if(myInterval > 0) {
-      clearInterval(myInterval)
+    if (selectCountry instanceof HTMLSelectElement) {
+      const selectCountryISO: HTMLSelectElement = selectCountry;
+      const selectCountryISOVal: string = selectCountryISO.value;
+      const tests = await weatherFunction(test, selectCountryISOVal);
+      dataVal.push(tests);
     } else {
-      console.error("Failed to clear the interval")
+      console.log(false);
     }
-  },7000)
-  
-
-
-
-    if(selectCountry instanceof HTMLSelectElement) {
-      const selectCountryISO: HTMLSelectElement = selectCountry
-      const selectCountryISOVal: string = selectCountryISO.value
-      const tests = await weatherFunction(test,selectCountryISOVal)
-      console.log(tests)
-    } else {
-      console.log(false)
-    }
-  })
+  });
 } else {
-  console.log("This is not button", false)
+  console.log("This is not button", false);
 }
